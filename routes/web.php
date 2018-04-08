5e8821de7b9905d43850731b5462daaa82c2586c
lee <ldt1.4.94@gmail.com>
2018-04-08 13:50:37 +0700

    task

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,4 @@
 <?php
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,4 +12,48 @@
 
 Route::get('/', function () {
     return view('welcome');
+
+});
+use App\Task;
+use Illuminate\Http\Request;
+
+
+    //Show Task Dashboard
+
+Route::get('/', function () {
+    $tasks = Task::orderBy('id', 'asc')->get();
+
+    return view('tasks', [
+        'tasks' => $tasks
+    ]);
+});
+
+
+    //Add New Task
+
+Route::post('/task', function (Request $request) {
+    $validator = Validator::make($request->all(), [
+        'name' => 'required|max:255',
+    ]);
+
+    if ($validator->fails()) {
+        return redirect('/')
+            ->withInput()
+            ->withErrors($validator);
+    }
+
+    $task = new Task;
+    $task->name = $request->name;
+    $task->save();
+
+    return redirect('/');
+});
+
+
+    //Delete Task
+
+Route::delete('/task/{task}', function (Task $task) {
+    $task->delete();
+
+    return redirect('/');
 });
